d905577c783ac1aa2f43d02d494dc89c8708bacf
jz0514-eng <jzheng0514@gmail.com>
2026-05-19 15:30:19 -0700

    tu8

--- tutorial/node-drawing-app/server.js
+++ tutorial/node-drawing-app/server.js
@@ -15,7 +15,7 @@ var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
-console.log(socket);
+    console.log('new connection: ' + socket.id);
 
 }
 
